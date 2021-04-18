a3e51f54d18226cb8468d28f723450f87c93fef5
Marek Galovic <galovic.galovic@gmail.com>
2021-04-18 15:09:54 +0200

    Abs cosine distance

--- index/space/space.go
+++ index/space/space.go
@@ -1,75 +1,74 @@
 package space
 
 import (
-    "github.com/marekgalovic/anndb/math";
+	"github.com/marekgalovic/anndb/math"
 
-    "github.com/klauspost/cpuid";
+	"github.com/klauspost/cpuid"
 )
 
 type SpaceImpl interface {
-    EuclideanDistance(math.Vector, math.Vector) float32
-    ManhattanDistance(math.Vector, math.Vector) float32
-    CosineDistance(math.Vector, math.Vector) float32
+	EuclideanDistance(math.Vector, math.Vector) float32
+	ManhattanDistance(math.Vector, math.Vector) float32
+	CosineDistance(math.Vector, math.Vector) float32
 }
 
 type Space interface {
-    Distance(math.Vector, math.Vector) float32
+	Distance(math.Vector, math.Vector) float32
 }
 
 type space struct {
-    impl SpaceImpl
+	impl SpaceImpl
 }
 
 func newSpace() space {
-    if cpuid.CPU.AVX() {
-        return space {impl: avxSpaceImpl{}}
-    }
-    if cpuid.CPU.SSE() {
-        return space {impl: sseSpaceImpl{}}
-    }
+	if cpuid.CPU.AVX() {
+		return space{impl: avxSpaceImpl{}}
+	}
+	if cpuid.CPU.SSE() {
+		return space{impl: sseSpaceImpl{}}
+	}
 
-    return space {impl: nativeSpaceImpl{}}
+	return space{impl: nativeSpaceImpl{}}
 }
 
-type Euclidean struct { space }
+type Euclidean struct{ space }
 
-type Manhattan struct { space }
+type Manhattan struct{ space }
 
-type Cosine struct { space }
+type Cosine struct{ space }
 
 func NewEuclidean() Space {
-    return &Euclidean{newSpace()}
+	return &Euclidean{newSpace()}
 }
 
 func (this *Euclidean) Distance(a, b math.Vector) float32 {
-    return this.impl.EuclideanDistance(a, b)
+	return this.impl.EuclideanDistance(a, b)
 }
 
 func (this *Euclidean) String() string {
-    return "euclidean"
+	return "euclidean"
 }
 
 func NewManhattan() Space {
-    return &Manhattan{newSpace()}
+	return &Manhattan{newSpace()}
 }
 
 func (this *Manhattan) Distance(a, b math.Vector) float32 {
-    return this.impl.ManhattanDistance(a, b)
+	return this.impl.ManhattanDistance(a, b)
 }
 
 func (this *Manhattan) String() string {
-    return "manhattan"
+	return "manhattan"
 }
 
 func NewCosine() Space {
-    return &Cosine{newSpace()}
+	return &Cosine{newSpace()}
 }
 
 func (this *Cosine) Distance(a, b math.Vector) float32 {
-    return this.impl.CosineDistance(a, b)
+	return math.Abs(this.impl.CosineDistance(a, b))
 }
 
 func (this *Cosine) String() string {
-    return "cosine"
+	return "cosine"
 }
-
--- math/math.go
+++ math/math.go
@@ -1,12 +1,12 @@
 package math
 
 import (
-    goMath "math";
+	goMath "math"
 )
 
 var (
-    parallelThreshold = 100000
-    numRoutines = 4
+	parallelThreshold = 100000
+	numRoutines       = 4
 )
 
 func SetParallelThreshold(threshold int) { parallelThreshold = threshold }
@@ -14,74 +14,74 @@ func SetParallelThreshold(threshold int) { parallelThreshold = threshold }
 func SetNumRoutines(n int) { numRoutines = n }
 
 const MaxFloat = float32(goMath.MaxFloat32)
-const MaxIntVal = int((^uint(0)) >> 1) 
+const MaxIntVal = int((^uint(0)) >> 1)
 const MinIntVal = -MaxIntVal - 1
 
 func Abs(x float32) float32 {
-    return float32(goMath.Abs(float64(x)))
+	return float32(goMath.Abs(float64(x)))
 }
 
 func Pow(x, power float32) float32 {
-    // Slow
-    return float32(goMath.Pow(float64(x), float64(power)))
+	// Slow
+	return float32(goMath.Pow(float64(x), float64(power)))
 }
 
 func Square(x float32) float32 {
-    return x * x
+	return x * x
 }
 
 func Sqrt(x float32) float32 {
-    return float32(goMath.Sqrt(float64(x)))
+	return float32(goMath.Sqrt(float64(x)))
 }
 
 func Log(x float32) float32 {
-    return float32(goMath.Log(float64(x)))
+	return float32(goMath.Log(float64(x)))
 }
 
 func Trunc(x float32) int {
-    return int(goMath.Trunc(float64(x)))
+	return int(goMath.Trunc(float64(x)))
 }
 
 func Floor(x float32) int {
-    return int(goMath.Floor(float64(x)))
+	return int(goMath.Floor(float64(x)))
 }
 
 func Min(values ...float32) float32 {
-    min := MaxFloat
-    for _, value := range values {
-        if value < min {
-            min = value
-        }
-    }
-    return min
+	min := MaxFloat
+	for _, value := range values {
+		if value < min {
+			min = value
+		}
+	}
+	return min
 }
 
 func MinInt(values ...int) int {
-    min := MaxIntVal
-    for _, value := range values {
-        if value < min {
-            min = value
-        }
-    }
-    return min
+	min := MaxIntVal
+	for _, value := range values {
+		if value < min {
+			min = value
+		}
+	}
+	return min
 }
 
 func Max(values ...float32) float32 {
-    max := -MaxFloat
-    for _, value := range values {
-        if value > max {
-            max = value
-        }
-    }
-    return max
+	max := -MaxFloat
+	for _, value := range values {
+		if value > max {
+			max = value
+		}
+	}
+	return max
 }
 
 func MaxInt(values ...int) int {
-    max := -MaxIntVal
-    for _, value := range values {
-        if value > max {
-            max = value
-        }
-    }
-    return max
+	max := -MaxIntVal
+	for _, value := range values {
+		if value > max {
+			max = value
+		}
+	}
+	return max
 }
